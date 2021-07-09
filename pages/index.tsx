00b4792c5d0d363140642c84dc94d15831a6df93
imalbert <12537973+imalbert@users.noreply.github.com>
2021-07-10 06:54:18 +0800

    divide body content into sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,95 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import ArrowDownIcon from "../images/icon-arrow-down.svg";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      About Services Projects Contact We are creatives Transform your brand We
-      are a full-service creative agency specializing in helping brands grow
-      fast. Engage your clients through compelling visuals that do most of the
-      marketing for you. Learn more Stand out to the right audience Using a
-      collaborative formula of designers, researchers, photographers,
-      videographers, and copywriters, we’ll build and extend your brand in
-      digital places. Learn more Graphic design Great design makes you
-      memorable. We deliver artwork that underscores your brand message and
-      captures potential clients’ attention. Photography Increase your
-      credibility by getting the most stunning, high-quality photos that improve
-      your business image. Client testimonials We put our trust in Sunnyside and
-      they delivered, making sure our needs were met and deadlines were always
-      hit. Emily R. Marketing Director Sunnyside’s enthusiasm coupled with their
-      keen interest in our brand’s success made it a satisfying and enjoyable
-      experience. Thomas S. Chief Operating Officer Incredible end result! Our
-      sales increased over 400% when we worked with Sunnyside. Highly
-      recommended! Jennie F. Business Owner About Services Projects Contact
-      <div className="attribution">
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
-    </div>
+    <>
+      <nav>About Services Projects Contact</nav>
+      <main>
+        <section className={styles.hero}>
+          <h1>We are creatives</h1>
+          <Image src={ArrowDownIcon} alt="" />
+        </section>
+        <section>
+          <h2>Transform your brand</h2>
+          <p>
+            We are a full-service creative agency specializing in helping brands
+            grow fast. Engage your clients through compelling visuals that do
+            most of the marketing for you.
+          </p>
+          Learn more
+        </section>
+        <section>
+          <h2>Transform your brand</h2>
+          <p>
+            Stand out to the right audience Using a collaborative formula of
+            designers, researchers, photographers, videographers, and
+            copywriters, we’ll build and extend your brand in digital places.
+          </p>
+          Learn more
+        </section>
+        <section>
+          <h2>Graphic design</h2>
+          <p>
+            Great design makes you memorable. We deliver artwork that
+            underscores your brand message and captures potential clients’
+            attention.
+          </p>
+        </section>
+        <section>
+          <h2>Photography</h2>
+          <p>
+            Increase your credibility by getting the most stunning, high-quality
+            photos that improve your business image.
+          </p>
+        </section>
+        <section>
+          <h3>Client testimonials</h3>
+          <div>
+            <img src="" alt="" />
+            <p>
+              We put our trust in Sunnyside and they delivered, making sure our
+              needs were met and deadlines were always hit.
+            </p>
+            <h4>Emily R.</h4>
+            <span>Marketing Director</span>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>
+              Sunnyside’s enthusiasm coupled with their keen interest in our
+              brand’s success made it a satisfying and enjoyable experience.
+            </p>
+            <h4>Thomas S. Chief</h4>
+            <span>Operating Officer</span>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>
+              Incredible end result! Our sales increased over 400% when we
+              worked with Sunnyside. Highly recommended!
+            </p>
+            <h4>Jennie F.</h4>
+            <span>Business Owner</span>
+          </div>
+        </section>
+        <img src="" alt="" />
+        <footer>About Services Projects Contact</footer>
+
+        <div className="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </main>
+    </>
   );
 }
